e526cd1001e9364376bfc7f7dd6b41aff7c7d9bb
Bruno de Lima <dlimaun@gmail.com>
2017-07-12 19:33:26 -0300

    Paulinha

--- app/src/androidTest/java/pointlessgroup/pointless/TimeRecordActivityTest.kt
+++ app/src/androidTest/java/pointlessgroup/pointless/TimeRecordActivityTest.kt
@@ -5,16 +5,27 @@ import android.support.test.espresso.Espresso.*
 import android.support.test.espresso.assertion.ViewAssertions
 import android.support.test.espresso.matcher.ViewMatchers
 import android.support.test.espresso.matcher.ViewMatchers.*
+import android.support.test.rule.ActivityTestRule
 import org.hamcrest.CoreMatchers
+import org.junit.Rule
 import org.junit.Test
+import org.junit.runner.RunWith
+import org.junit.runners.JUnit4
 
-
+@RunWith(JUnit4::class)
 class TimeRecordActivityTest {
 
+    @JvmField @Rule val rule = ActivityTestRule<TimeRecordActivity>(TimeRecordActivity::class.java, true, true)
+
     @Test fun whenCreated_ShouldShowTimerRecordButton() {
+
         onView(withId(R.id.btn_mainactivity_timerecord))
                 .check(ViewAssertions.matches(isDisplayed()))
     }
+
+    @Test fun whenRecordShould
+
+
 
 
 }
