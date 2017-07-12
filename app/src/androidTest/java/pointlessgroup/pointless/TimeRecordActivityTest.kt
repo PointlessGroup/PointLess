bf0c0e078775db7407cfbb1c642dad62764222fc
Bruno de Lima <dlimaun@gmail.com>
2017-07-12 19:25:36 -0300

    leonardo

--- app/src/androidTest/java/pointlessgroup/pointless/TimeRecordActivityTest.kt
+++ app/src/androidTest/java/pointlessgroup/pointless/TimeRecordActivityTest.kt
@@ -2,17 +2,19 @@ package pointlessgroup.pointless
 
 import android.support.test.espresso.Espresso
 import android.support.test.espresso.Espresso.*
+import android.support.test.espresso.assertion.ViewAssertions
 import android.support.test.espresso.matcher.ViewMatchers
 import android.support.test.espresso.matcher.ViewMatchers.*
 import org.hamcrest.CoreMatchers
 import org.junit.Test
 
 
-class MainActivitytest {
-    
+class TimeRecordActivityTest {
+
     @Test fun whenCreated_ShouldShowTimerRecordButton() {
         onView(withId(R.id.btn_mainactivity_timerecord))
-                .check(CoreMatchers.matches(isDisplayed()))
+                .check(ViewAssertions.matches(isDisplayed()))
     }
+
 
 }
---
+++ app/src/main/java/pointlessgroup/pointless/TimeRecordActivity.kt
@@ -0,0 +1,12 @@
+package pointlessgroup.pointless
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class TimeRecordActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_time_record)
+    }
+}
