8bc522acaa2c8f010a62aa3e49abdecf5a4c56c5
Bruno de Lima <dlimaun@gmail.com>
2017-10-19 09:08:45 -0200

    Add dependencies for Geofence and ArchtectureComponents

---
+++ app/src/main/java/pointlessgroup/pointless/GeofenceTransitionsIntentService.kt
@@ -0,0 +1,10 @@
+package pointlessgroup.pointless;
+
+import android.app.IntentService
+import android.content.Intent
+
+class GeofenceTransitionsIntentService : IntentService("GeofenceService") {
+
+    override fun onHandleIntent(intent: Intent?) {
+    }
+}
