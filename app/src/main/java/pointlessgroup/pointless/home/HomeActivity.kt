cd0ff6e6b7b59b8ecc820e9102a1063eff0a0a4c
Bruno de Lima <dlimaun@gmail.com>
2017-10-24 08:04:03 -0200

    Refactor homeActivity to improve the way to we create and bind to fragments
    * This will avoid fragments re-creation
    * Also adds settings fragment

--- app/src/main/java/pointlessgroup/pointless/home/HomeActivity.kt
+++ app/src/main/java/pointlessgroup/pointless/home/HomeActivity.kt
@@ -10,23 +10,36 @@ import pointlessgroup.pointless.databinding.ActivityHomeBinding
 class HomeActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityHomeBinding
+    private val fragments = HashMap<String, Fragment>(3)
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = DataBindingUtil.setContentView(this, R.layout.activity_home)
         binding.bottomNavigation.setOnNavigationItemSelectedListener { item ->
             when (item.itemId) {
-                R.id.reports -> addFragment(DashboardFragment(), "reports")
-                R.id.dashboard -> addFragment(DashboardFragment(), "dashboard")
-                R.id.settings -> addFragment(DashboardFragment(), "settings")
-                else -> addFragment(DashboardFragment(), "dashboard")
+                R.id.reports -> switchFragment("reports") { DashboardFragment() }
+                R.id.dashboard -> switchFragment("dashboard") { DashboardFragment() }
+                R.id.settings -> switchFragment("settings") { SettingsFragment() }
+                else -> switchFragment("dashboard") { DashboardFragment() }
             }
             true
         }
         binding.bottomNavigation.selectedItemId = R.id.dashboard
     }
 
-    private fun addFragment(fragment: Fragment, tag: String) {
+    private fun switchFragment(tag: String, creator: () -> Fragment) {
+        replaceFragment(getOrCreate(tag, creator), tag)
+    }
+
+    private fun getOrCreate(tag: String, creator: () -> Fragment): Fragment {
+        if (fragments.containsKey(tag) && fragments[tag] != null)
+            return fragments[tag]!!
+        val fragment = creator.invoke()
+        fragments[tag] = fragment
+        return fragment
+    }
+
+    private fun replaceFragment(fragment: Fragment, tag: String) {
         supportFragmentManager.beginTransaction()
                 .replace(R.id.container, fragment, tag)
                 .commit()
